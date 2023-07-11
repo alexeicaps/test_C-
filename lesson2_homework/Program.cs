28bbfd0d5fb728978a45de406a2b1634f185cbcd
Alexei Capsamun <caps.alex@mail.ru>
2023-07-11 16:55:38 +0300

    update lesson2_homework

--- lesson2_homework/Program.cs
+++ lesson2_homework/Program.cs
@@ -3,30 +3,61 @@
 // 782 -> 8
 // 918 -> 1
 
+
+// Console.Write("Введите трёхзначное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// void GetSecondDigit(int number1)
+// {
+//     if (number1 >= 100 && number1 < 1000)
+//     {
+//         int second = (number1 % 100 - number1 % 10) / 10;
+//         Console.WriteLine(second);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Это число не трехзначное");
+//     }
+// }
+
+// GetSecondDigit(number);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int ThirdDigit(int number1)
+{
+    // int result = -1;
+    int result = int.Parse("Третьей цифры нет");
+    if (number1 >= 100)
+    {
+        while (number1 > 999)
+        {
+            number1 = number1 / 10;
+        }
+        result = number1 % 10;
+    }
+    return result; 
+    // else
+    // {
+    //     Console.WriteLine("Третьей цифры нет");
+    // }
+}
+int num = ThirdDigit(number);
+Console.WriteLine(num);
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Введите трёхзначное число: ");
-int number = int.Parse(Console.ReadLine()!);
-
-void GetSecondDigit(int number1)
-{
-    if (number1 >= 100 && number1 < 1000)
-    {
-        int second = (number1 % 100 - number1 % 10) / 10;
-        Console.WriteLine(second);
-    }
-    else
-    {
-        Console.WriteLine("Это число не трехзначное");
-    }
-}
-
-GetSecondDigit(number);
