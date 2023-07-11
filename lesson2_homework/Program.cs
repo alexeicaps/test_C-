e90e0727a9553761402541496ee46ec150882959
alexeicaps <caps.alex@mail.ru>
2023-07-11 22:38:33 +0300

    added task3_1, task3_2, task3_3

--- lesson2_homework/Program.cs
+++ lesson2_homework/Program.cs
@@ -30,30 +30,85 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
 
-int ThirdDigit(int number1)
-{
-    // int result = -1;
-    int result = int.Parse("Третьей цифры нет");
-    if (number1 >= 100)
-    {
-        while (number1 > 999)
-        {
-            number1 = number1 / 10;
-        }
-        result = number1 % 10;
-    }
-    return result; 
-    // else
-    // {
-    //     Console.WriteLine("Третьей цифры нет");
-    // }
-}
-int num = ThirdDigit(number);
-Console.WriteLine(num);
+// int ThirdDigit(int number1)
+// {
+//     // int result = -1;
+//     int result = int.Parse("Третьей цифры нет");
+//     if (number1 >= 100)
+//     {
+//         while (number1 > 999)
+//         {
+//             number1 = number1 / 10;
+//         }
+//         result = number1 % 10;
+//     }
+//     return result; 
+//     // else
+//     // {
+//     //     Console.WriteLine("Третьей цифры нет");
+//     // }
+// }
+// int num = ThirdDigit(number);
+// Console.WriteLine(num);
 
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// string mess = "Введите число: ";
+// int number1 = Prompt(mess);
+// int number2 = Prompt("Введите пятизначное число");
+
+// int result1 = FindThirdNumber(number1);
+// int result2 = FindThirdNumber(number2);
+
+// GetResult(result1);
+// GetResult(result2);
+
+
+
+// void GetResult(int res)
+// {
+//     if (res == -1)
+//     {
+//         Console.WriteLine("Такой цифры нет! ");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Третья цифра числа {res}");
+//     }
+// }
+
+
+
+// int Prompt(string message)
+// {
+//     Console.Clear();
+//     Console.WriteLine(message);
+//     int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//     return num;
+// }
+
+// int FindThirdNumber(int num)
+// {
+//     if (num < 100)
+//     {
+//         return -1;
+
+//     }
+//     else
+//     {
+//         while (num >= 1000)
+//         {
+//             num = num / 10;
+//         }
+//         int thirdNumber = num % 10;
+//         return thirdNumber;
+//     }
+// }
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
---
+++ task3_2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// int Prompt(string message)
+
+// {
+//     Console.Write(message);
+//     int num = int.Parse(Console.ReadLine());
+//     return num;
+// }
+// int num1 = Prompt("введите четверть");
+// GetResult(num1);
+// void GetResult (int num1)
+// {
+
+//     if (num1 == 1)
+//     {
+//       Console.WriteLine("X>0, Y>0");
+//     }
+//     else if (num1 == 2)
+//     {
+//         Console.WriteLine("X<0, Y>0");
+//     }
+//     else if (num1 == 3)
+//     {
+//        Console.WriteLine("X<0, Y<0");
+//     }
+//     else if (num1 == 4)
+//     {
+//         Console.WriteLine("X>0, Y<0");
+//     }
+// }
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// GetSquare();
+
+// void GetSquare()
+// {
+//         Console.Write("x1 - ");
+//         int x1 = int.Parse(Console.ReadLine());
+
+//         Console.Write("y1 - ");
+//         int y1 = int.Parse(Console.ReadLine());
+
+//         Console.Write("x2 - ");
+//         int x2 = int.Parse(Console.ReadLine());
+
+//         Console.Write("y2 - ");
+//         int y2 = int.Parse(Console.ReadLine());
+
+//         double a = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+//         Console.WriteLine(Math.Round(a,2));
+// }
+
+
