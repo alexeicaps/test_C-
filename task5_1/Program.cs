f6a6b11b35a122d9374b7a1fe2ae0c6b28c00e58
alexeicaps <caps.alex@mail.ru>
2023-07-18 23:14:34 +0300

    Initialized task5_0.....

---
+++ task5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+{
+    static void Main(string[] args)
+    {
+        int[] array = { -4, -8, 8, 2 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+                array[i] = -array[i];
+            else if (array[i] < 0)
+                array[i] = -array[i];
+        }
+
+        Console.WriteLine("Измененный массив:");
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+}
---
+++ task5_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+void Main(string[] args)
+    {
+        int[] array = { 6, 7, 19, 345, 3 };
+        int targetNumber;
+
+        Console.Write("Введите число для поиска: ");
+        if (int.TryParse(Console.ReadLine(), out targetNumber))
+        {
+            if (ArrayContainsNumber(array, targetNumber))
+                Console.WriteLine("Да, число {0} присутствует в массиве.", targetNumber);
+            else
+                Console.WriteLine("Нет, число {0} отсутствует в массиве.", targetNumber);
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное число.");
+        }
+    }
+
+    static bool ArrayContainsNumber(int[] array, int target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == target)
+                return true;
+        }
+        return false;
+    }
+
+    Main(args);
