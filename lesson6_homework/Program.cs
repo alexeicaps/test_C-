e2b543a0e1e2dc68f134bb8b783ce801a6ed1205
Alexei Capsamun <caps.alex@mail.ru>
2023-07-28 10:18:55 +0300

    update task41

--- lesson6_homework/Program.cs
+++ lesson6_homework/Program.cs
@@ -3,68 +3,11 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-
-// int Prompt(string message)
-// {
-//     Console.Write(message);
-//     int num = int.Parse(Console.ReadLine()!);
-//     return num;
-// }
-
-// int length = Prompt("Введите колличество элементов: ");
-// int[] array;
-// array = GetArray(length);
-// // PrintArray(GetArray(length));
-// Console.WriteLine(($"Колличество положительных чисел -> {GetPositiveNumbers(array)}"));
-
-// int[] GetArray(int length)
-// {
-//     int[] array = new int[length];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         array[i] = Prompt($"Введите {i + 1}-й элемент: ");
-//     }
-//     return array;
-// }
-
-// void PrintArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.Write($"{arr[i]} ");
-//     }
-// }
-
-// int GetPositiveNumbers(int[] arr)
-// {
-//     int count = 0;
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         if (array[i] > 0)
-//         count += 1;
-//     }
-//     return count;
-// }
-
-
-
-
-
-
-
-// --------------------------------
-
-
-// Задача 41: Пользователь вводит с клавиатуры M чисел. 
-// Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
-
 int length = Prompt("Введите колличество элементов: ");
 int[] array;
 array = GetArray(length);
-// Console.WriteLine($"Колличество положительных чисел -> {GetPositiveNumbers(array)}");
-int numbers = PrintArray(array);
+PrintArray(array);
+Console.WriteLine($" -> {GetPositiveNumbers(array)}");
 
 
 int Prompt(string message)
@@ -79,7 +22,7 @@ int[] GetArray(int length)
     int[] array = new int[length];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = Prompt($"type the {i + 1}-th number: ");
+        array[i] = Prompt($"Введите {i + 1}-й элемент: ");
     }
     return array;
 }
@@ -98,6 +41,10 @@ void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");    
+        if (i == array.Length - 1) 
+        Console.Write($"{array[i]}");
+        else
+        Console.Write($"{array[i]}, ");    
     }
+    
 }
