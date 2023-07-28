ef5e2ec1f969a213758f2bf82162bc9f4e9896a5
Alexei Capsamun <caps.alex@mail.ru>
2023-07-28 16:45:49 +0300

    resolved task43

--- lesson6_homework/Program.cs
+++ lesson6_homework/Program.cs
@@ -3,48 +3,69 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-int length = Prompt("Введите колличество элементов: ");
-int[] array;
-array = GetArray(length);
-PrintArray(array);
-Console.WriteLine($" -> {GetPositiveNumbers(array)}");
+// int length = Prompt("Введите колличество элементов: ");
+// int[] array;
+// array = GetArray(length);
+// PrintArray(array);
+// Console.WriteLine($" -> {GetPositiveNumbers(array)}");
 
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    int num = int.Parse(Console.ReadLine()!);
-    return num;
-}
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     int num = int.Parse(Console.ReadLine()!);
+//     return num;
+// }
 
-int[] GetArray(int length)
-{
-    int[] array = new int[length];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = Prompt($"Введите {i + 1}-й элемент: ");
-    }
-    return array;
-}
+// int[] GetArray(int length)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = Prompt($"Введите {i + 1}-й элемент: ");
+//     }
+//     return array;
+// }
 
-int GetPositiveNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0) count = count + 1;
-    }
-    return count;
-}
+// int GetPositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0) count = count + 1;
+//     }
+//     return count;
+// }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i == array.Length - 1) 
-        Console.Write($"{array[i]}");
-        else
-        Console.Write($"{array[i]}, ");    
-    }
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == array.Length - 1) 
+//         Console.Write($"{array[i]}");
+//         else
+//         Console.Write($"{array[i]}, ");    
+//     }
     
-}
+// }
+
+
+// ----------------------------------------------------------
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+// Console.WriteLine("введите значение b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
