b3693afdf1cd65ad512c070aa9dc9252fad87d04
alexeicaps <caps.alex@mail.ru>
2023-08-01 23:09:20 +0300

    added task9_0 task9_1 task9_2

---
+++ task9_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int digitsSum(int number)
+{
+    if(number==0) return 0;
+    return number % 10 + digitsSum(number/10) * 2;
+}
+
+int sum = digitsSum(453);
+Console.WriteLine(sum);
