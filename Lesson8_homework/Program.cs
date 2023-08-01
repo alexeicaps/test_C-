d3a56d9e1b35a097819de56bea2586155f893355
Alexei Capsamun <caps.alex@mail.ru>
2023-08-01 16:48:15 +0300

    continue to resolve task56

--- Lesson8_homework/Program.cs
+++ Lesson8_homework/Program.cs
@@ -10,50 +10,172 @@
 // 8 4 4 2
 
 
-Console.Write("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество столбцов массива: ");
-int columns = int.Parse(Console.ReadLine()!);
-int[,] array = GetArray(rows, columns);
-Console.WriteLine();
-SortDecreaseRows(array);
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine()!);
+// int[,] array = GetArray(rows, columns);
+// Console.WriteLine();
+// SortDecreaseRows(array);
 
 
-int[,] GetArray(int row, int column)
+// int[,] GetArray(int row, int column)
+// {
+//     int[,] arr = new int[row, column];
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < column; j++)
+//         {
+//             arr[i, j] = new Random().Next(10, 50);
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     return arr;
+// }
+
+// void SortDecreaseRows(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             for (int k = j + 1; k < arr.GetLength(1); k++)
+//             {
+//                 // int maxValue = arr[i, j];
+//                 if (arr[i, j] < arr[i, k])
+//                 {
+//                     int temp = arr[i, j];
+//                     arr[i, j] = arr[i, k];
+//                     arr[i, k] = temp;
+//                 }
+
+//             }
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+
+// }
+
+
+// -------------------------------------------------------------------------------------------------------
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+int[,] array = GetArray(4, 6);
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n)
 {
-    int[,] arr = new int[row, column];
-    for (int i = 0; i < row; i++)
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < column; j++)
+        for (int j = 0; j < n; j++)
         {
-            arr[i, j] = new Random().Next(10, 50);
-            Console.Write($"{arr[i, j]} ");
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
     }
-    return arr;
 }
 
-void SortDecreaseRows(int[,] arr)
+int GetSumEveryRows(int[,] array, int i)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        int[,] arraySum = new int[array.GetLength(1), 0];
+        for (int i = 0; i < array.GetLength(1); i++)
         {
-            for (int k = j + 1; k < arr.GetLength(1); k++)
+            int result = 0;
+            for (int j = 0; j < array.GetLength(0); j++)
             {
-                // int maxValue = arr[i, j];
-                if (arr[i, j] < arr[i, k])
-                {
-                    int temp = arr[i, j];
-                    arr[i, j] = arr[i, k];
-                    arr[i, k] = temp;
-                }
-                
+                result = result + array.Length[j];
             }
-            Console.Write($"{arr[i, j]} ");
+            return result;
         }
-        Console.WriteLine();
+        return arraySum;
     }
-
 }
+
+GetSumEveryRows(array);
+
+
+
+
+// --------------------------------------------------------------------------
+
+// Console.Write("Введите размерность m массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите размерность n массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] randomArray = new int[m, n];
+
+// void mas(int m, int n)
+// {
+//     int i, j;
+//     Random rand = new Random();
+//     for (i = 0; i < m; i++)
+//     {
+//         for (j = 0; j < n; j++)
+//         {
+//             randomArray[i, j] = rand.Next(1, 9);
+//         }
+//     }
+// }
+
+// void printm(int[,] array)
+// {
+//     int i, j;
+//     for (i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.WriteLine();
+//         for (j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         // Console.WriteLine();
+//     }
+// }
+
+// mas(m, n);
+// Console.WriteLine("\nИсходный массив: ");
+// printm(randomArray);
+
+// // Функция, считающая сумму элементов в строке
+// int SumLine(int[,] array, int i)
+// {
+//     int sum = array[i, 0];
+//     for (int j = 1; j < array.GetLength(1); j++)
+//     {
+//         sum += array[i, j];
+//     }
+//     return sum;
+// }
+
+// int minSum = 1;
+// int sum = SumLine(randomArray, 0);
+// for (int i = 1; i < randomArray.GetLength(0); i++)
+// {
+//     if (sum > SumLine(randomArray, i))
+//     {
+//         sum = SumLine(randomArray, i);
+//         minSum = i + 1;
+//     }
+// }
+// Console.WriteLine($"\nСтрока c наименьшей суммой элементов: {minSum}");
