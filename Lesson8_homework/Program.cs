cc2dee27af50526d5291956ef678620c61f0d2f0
alexeicaps <caps.alex@mail.ru>
2023-08-04 00:20:13 +0300

    Resolved task58

--- Lesson8_homework/Program.cs
+++ Lesson8_homework/Program.cs
@@ -146,15 +146,15 @@ Console.Write("Введите размерность m массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите размерность n массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите размерность k массива: ");
-int k = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите размерность l массива: ");
-int l = Convert.ToInt32(Console.ReadLine());
-int[,] randomArray1 = new int[m, n];
-int[,] randomArray2 = new int[k, l];
-mas(m, n);
-Console.WriteLine("\nИсходный массив: ");
-PrintArray($"{PrintArray} -");
+int[,] randomArray1 = GetArray(m, n);
+int[,] randomArray2 = GetArray(m, n);
+Console.WriteLine("\nДаны 2 матрицы: ");
+PrintArray(randomArray1);
+Console.WriteLine();
+PrintArray(randomArray2);
+int[,] resultArray = GetMultiplyMatrix(randomArray1, randomArray2);
+Console.WriteLine("\nПроизведение двух матриц: ");
+PrintArray(resultArray);
 
 
 int[,] GetArray(int m, int n)
@@ -164,7 +164,7 @@ int[,] GetArray(int m, int n)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = new Random().Next(0, 10);
+            array[i, j] = new Random().Next(0, 5);
         }
     }
     return array;
@@ -182,8 +182,21 @@ void PrintArray(int[,] array)
     }
 }
 
-
-
+int[,] GetMultiplyMatrix(int[,] array1, int[,] array2)
+{
+    int[,] resultArr = new int[array1.GetLength(0), array1.GetLength(1)];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                resultArr[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return resultArr;
+}
 
 
 
