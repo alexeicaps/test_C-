fbed6cfad5f841e3e6cc2b39e7691c0d6afe1a43
Alexei Capsamun <caps.alex@mail.ru>
2023-08-08 16:23:11 +0300

    resolved all task lesson9_homework

--- lesson9_homework/Program.cs
+++ lesson9_homework/Program.cs
@@ -49,16 +49,50 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-Console.Write("Введите значение M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите неотрицательное число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"m = {m}, n = {n} -> A(m, n) = {GetAkkerman(m, n)} ");
 
-int akkerman(int m, int n)
+
+int GetAkkerman(int m, int n)
 {
-if (m == 0) return n + 1;
-else if (n == 0) return akkerman(m - 1, 1);
-else return akkerman(m - 1, akkerman(m, n - 1));
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return GetAkkerman(m - 1, 1);
+    else return GetAkkerman(m - 1, GetAkkerman(m, n - 1));
 }
 
-Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("type m: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("type n: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write($"m = {m}, n = {n} -> A(m, n) = {GetAkkerman(m, n)} ");
+
+
+// int GetAkkerman(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (m > 0 && n == 0) return GetAkkerman(m - 1, 1);
+//     else return GetAkkerman(m - 1, GetAkkerman(m, n - 1));
+// }
+
+// int akkerman(int m, int n)
+// {
+// if (m == 0) return n + 1;
+// else if (n == 0) return akkerman(m - 1, 1);
+// else (m > 0 && n > 0) return akkerman(m - 1, akkerman(m, n - 1)); ??? 
+// }
+
+// Console.Write($"-> {akkerman(m, n)} ");
