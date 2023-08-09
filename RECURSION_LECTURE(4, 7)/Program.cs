eed2700530372b27f124647023ba7e9b3469a3fc
Alexei Capsamun <caps.alex@mail.ru>
2023-08-09 16:25:53 +0300

    update recursion_lecture

--- RECURSION_LECTURE(4, 7)/Program.cs
+++ RECURSION_LECTURE(4, 7)/Program.cs
@@ -131,21 +131,38 @@
 
 // Fibonacci
 
-// f(1) = 1
-// f(2) = 1
-// f(n) = f(n -1) + f(n - 2)
+// // f(1) = 1
+// // f(2) = 1
+// // f(n) = f(n -1) + f(n - 2)
 
-double Fibonacci(int n)
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
+
+// // int n = Fibonacci(6);
+// // Console.WriteLine(n);
+
+
+
+
+// -------------------------------------------------------------------------
+// --------find directory----------
+
+string path = "D:/GeekBrains/C#/Seminars/test_C-/task0";
+DirectoryInfo di = new DirectoryInfo(path);
+Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
 {
-    if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+    Console.WriteLine(fi[i].Name);
 }
-
-for (int i = 1; i < 50; i++)
-{
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
-}
-
-
-// int n = Fibonacci(6);
-// Console.WriteLine(n);
